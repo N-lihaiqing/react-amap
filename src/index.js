703e96e49b9b57f4212595336ed2945b45625bdd
Siwen <quiet0523@163.com>
2018-12-17 15:52:04 +0800

    增加地图控件

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import ReactMap from './react-amap/ReactMap';
 import registerServiceWorker from './registerServiceWorker';
+import AMapPage from "./AMapPage";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<AMapPage />, document.getElementById('root'));
 registerServiceWorker();
